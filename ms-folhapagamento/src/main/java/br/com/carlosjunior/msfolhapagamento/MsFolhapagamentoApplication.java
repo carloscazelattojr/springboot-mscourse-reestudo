0b09fd27803d1c37a48c72dc4cc3ecff86a5983f
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2021-12-13 07:00:13 -0300

    Projeto Folha de Pagamento criado

---
+++ ms-folhapagamento/src/main/java/br/com/carlosjunior/msfolhapagamento/MsFolhapagamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.carlosjunior.msfolhapagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFolhapagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFolhapagamentoApplication.class, args);
+	}
+
+}
---
+++ ms-folhapagamento/src/test/java/br/com/carlosjunior/msfolhapagamento/MsFolhapagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carlosjunior.msfolhapagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFolhapagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
